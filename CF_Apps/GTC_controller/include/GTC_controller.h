18d875685b4c784f46b8231a87462f086eb99a73
bhabas <bhabas@psu.edu>
2023-04-09 20:04:48 -0400

    Creating header file

--- CF_Apps/GTC_controller/include/GTC_controller.h
+++ CF_Apps/GTC_controller/include/GTC_controller.h
@@ -1,4 +1,47 @@
-#pragma once
+/**
+ *
+ * controller_gtc.h - Geometric Tracking Controller Interface
+ *
+ */
+
+#ifndef __CONTROLLER_GTC_H__
+#define __CONTROLLER_GTC_H__
+
+#ifdef GAZEBO_SIM
+#define consolePrintf printf
+#define DEBUG_PRINT printf
+#endif
+
+// STANDARD LIBRARIES
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+// CF LIBARARIES
+#include "math3d.h"
+#include "log.h"
+#include "param.h"
+#include "debug.h"
+#include "motors.h"
+#include "pm.h"
+#include "led.h"
+
+
+#include "app.h"
+#include "app_channel.h"
+#include "controller.h"
+
+
+#define DEBUG_MODULE "GTC_CONTROLLER"
+
 
 float value_1 = 5.4f;
 float value_2 = 3.14f;
+
+
+
+
+#endif //__CONTROLLER_GTC_H__
--- CF_Apps/GTC_controller/src/GTC_controller.c
+++ CF_Apps/GTC_controller/src/GTC_controller.c
@@ -1,28 +1,5 @@
-
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-#include "app.h"
-#include "app_channel.h"
-
-#include "FreeRTOS.h"
-#include "task.h"
-
-#define DEBUG_MODULE "GTC_CONTROLLER"
-#include "debug.h"
-#include "led.h"
-
-#include "log.h"
-#include "param.h"
-#include "math3d.h"
-
-#include "controller.h"
 #include "GTC_controller.h"
 
-float Theta_x_cam_est = 0.0f;
-float Theta_y_cam_est = 0.0f;
-float Theta_z_cam_est = 0.0f;
 
 struct GTC_CmdPacket{
     uint8_t cmd_type; 
@@ -30,25 +7,29 @@ struct GTC_CmdPacket{
     float cmd_val2;
     float cmd_val3;
     float cmd_flag;
+    bool  cmd_rx;
 } __attribute__((packed));
 
-struct GTC_CmdPacket GTC_Cmd;
+extern struct GTC_CmdPacket GTC_Cmd;
 
 void appMain() {
-    DEBUG_PRINT("Waiting for activation ...\n");
 
+    // while(1) {
 
-    
-
-    while(1) {
-
-        if (appchannelReceiveDataPacket(&GTC_Cmd,sizeof(GTC_Cmd),APPCHANNEL_WAIT_FOREVER))
-        {
-
-            // consolePrintf("App channel received x: %.3f\n",(double)GTC_Cmd.cmd_val3);
-
-        }
-    }
+    //     // #ifdef GAZEBO_SIM
+    //     // // EXECUTE GTC COMMAND WHEN RECEIVED
+    //     // if (GTC_Cmd.cmd_rx == true)
+    //     // {
+    //     //     // GTC_Command(&GTC_Cmd);
+    //     // }
+    //     // #else
+    //     // // WAIT UNTIL GTC COMMAND IS RECEIVED
+    //     // if (appchannelReceiveDataPacket(&GTC_Cmd,sizeof(GTC_Cmd),APPCHANNEL_WAIT_FOREVER))
+    //     // {
+    //     //     // if (GTC_Cmd.cmd_rx == true) GTC_Command(&GTC_Cmd);
+    //     // }
+    //     // #endif
+    // }
   
 }
 
@@ -73,15 +54,7 @@ void controllerOutOfTree(control_t *control,const setpoint_t *setpoint,
 
     if (RATE_DO_EXECUTE(2, tick))
     {
-        // DEBUG_PRINT("Hello Custom Controller!\n");
-
-
-
-        #ifdef EXP_FLAG
-        DEBUG_PRINT("Param Value: %.3f\n",(double)value_1);
-        DEBUG_PRINT("Log Value: %.3f\n",(double)value_2);
-        #endif
-
+        DEBUG_PRINT("Hello Custom Controller!\n");
 
 
     }
@@ -89,10 +62,12 @@ void controllerOutOfTree(control_t *control,const setpoint_t *setpoint,
 
 }
 
-PARAM_GROUP_START(my_PARAM)
-    PARAM_ADD(PARAM_FLOAT | PARAM_PERSISTENT, value_1, &value_1)
-PARAM_GROUP_STOP(my_PARAM)
+#ifndef GAZEBO_SIM
+// PARAM_GROUP_START(my_PARAM)
+//     PARAM_ADD(PARAM_FLOAT | PARAM_PERSISTENT, value_1, &value_1)
+// PARAM_GROUP_STOP(my_PARAM)
 
-LOG_GROUP_START(my_LOG)
-    LOG_ADD(LOG_FLOAT, value_2, &value_2)
-LOG_GROUP_STOP(my_LOG)
+// LOG_GROUP_START(my_LOG)
+//     LOG_ADD(LOG_FLOAT, value_2, &value_2)
+// LOG_GROUP_STOP(my_LOG)
+#endif
