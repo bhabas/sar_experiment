83cb97ce7f717709b7f8a63edd6f1b924a44432e
bhabas <bhabas@psu.edu>
2023-04-11 19:51:12 -0400

    Starting to reimplement more controller commands

--- CF_Apps/controller_GTC/include/controller_GTC.h
+++ CF_Apps/controller_GTC/include/controller_GTC.h
@@ -34,3 +34,39 @@ void controllerOutOfTreeReset();
 #ifdef __cplusplus
 }
 #endif
+#pragma once
+
+#ifdef __cplusplus // If C++ compiler then compile accordingly
+extern "C" {
+#endif
+
+// STANDARD LIBRARIES
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+// CF LIBARARIES
+#include "app.h"
+#include "stabilizer_types.h"
+
+#include "console.h"
+
+// CUSTOM LIBRARIES
+#include "shared_lib.h"
+
+
+#ifdef CONFIG_SAR_EXP
+#include "FreeRTOS.h"
+#include "task.h"
+#include "app_channel.h"
+#endif
+
+
+void controllerOutOfTreeReset();
+
+
+#ifdef __cplusplus
+}
+#endif
--- CF_Apps/controller_GTC/src/shared_lib.c
+++ CF_Apps/controller_GTC/src/shared_lib.c
@@ -23,20 +23,20 @@ float dt = (float)(1.0f/RATE_100_HZ);
 // (INITIAL VALUES THAT ARE OVERWRITTEN BY Ctrl_Gains.yaml)
 
 // XY POSITION PID
-float P_kp_xy = 0.5f;
-float P_kd_xy = 0.3f;
-float P_ki_xy = 0.0f;
+float P_kp_xy = 0.4f;
+float P_kd_xy = 0.35f;
+float P_ki_xy = 0.07f;
 float i_range_xy = 0.3f;
 
 // Z POSITION PID
 float P_kp_z = 1.20f;
 float P_kd_z = 0.35f;
-float P_ki_z = 0.0f;
+float P_ki_z = 0.1f;
 float i_range_z = 0.25f;
 
 // XY ATTITUDE PID
-float R_kp_xy = 0.02f;
-float R_kd_xy = 0.008f;
+float R_kp_xy = 0.004f;
+float R_kd_xy = 0.0017f;
 float R_ki_xy = 0.0f;
 float i_range_R_xy = 1.0f;
 
@@ -184,8 +184,35 @@ void GTC_Command(struct GTC_CmdPacket *GTC_Cmd)
 
         case 1: // Position
 
-            consolePrintf("Cmd Pos: %.3f\n",(double)GTC_Cmd->cmd_val1);
+            consolePrintf("Pos Val: %.3f\n",(double)GTC_Cmd->cmd_val1);
+            x_d.x = GTC_Cmd->cmd_val1;
+            x_d.y = GTC_Cmd->cmd_val2;
+            x_d.z = GTC_Cmd->cmd_val3;
+            kp_xf = 1.0f;
             break;
+        
+        case 2: // Velocity
+            v_d.x = GTC_Cmd->cmd_val1;
+            v_d.y = GTC_Cmd->cmd_val2;
+            v_d.z = GTC_Cmd->cmd_val3;
+            kd_xf = GTC_Cmd->cmd_flag;
+            break;
+
+        case 3: 
+
+            /* Do Nothing */
+            break;
+
+        case 4: // Euler Angle
+
+            // TODO: ADD ANGLE SETPOINT OPTION INTO CONTROLLER FOR ANGLE BASED POLICY
+
+            break;        
+
+        case 5: // Hard Set All Motorspeeds to Zero
+            motorstop_flag = true;
+            break;
+
    
     }
 
