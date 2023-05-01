e260770fe7614616217f0ecd914d8420beeb7072
bhabas <bhabas@psu.edu>
2023-05-01 10:15:36 -0400

    Cleaning to barebones example

--- AI_Deck_Apps/cam_test/cam_test.c
+++ AI_Deck_Apps/cam_test/cam_test.c
@@ -1,70 +1,37 @@
 /* PMSIS includes */
 #include "pmsis.h"
 
-/* Task executed by cluster cores. */
-void cluster_helloworld(void *arg)
-{
-    uint32_t core_id = pi_core_id(), cluster_id = pi_cluster_id();
-    printf("[%d %d] Hello World!\n", cluster_id, core_id);
-}
+#include "bsp/bsp.h"
+#include "bsp/camera.h"
+#include "bsp/camera/himax.h"
+#include "bsp/buffer.h"
+#include "stdio.h"
 
-/* Cluster main entry, executed by core 0. */
-void cluster_delegate(void *arg)
-{
-    printf("Cluster master core entry\n");
-    /* Task dispatch to cluster cores. */
-    pi_cl_team_fork(pi_cl_cluster_nb_cores(), cluster_helloworld, arg);
-    printf("Cluster master core exit\n");
-}
+#define WIDTH 324
+#define HEIGHT 244
+#define BUFF_SIZE (WIDTH*HEIGHT)
+PI_L2 unsigned char *buff;
 
-void helloworld(void)
+static struct pi_device camera;
+
+void test_camera(void)
 {
     printf("Entering main controller\n");
+    pi_freq_set(PI_FREQ_DOMAIN_FC, 50000000);
 
-    uint32_t errors = 0;
-    uint32_t core_id = pi_core_id(), cluster_id = pi_cluster_id();
-    printf("[%d %d] Hello World!\n", cluster_id, core_id);
+    printf("Testing normal camera capture\n");
 
-    struct pi_device cluster_dev;
-    struct pi_cluster_conf cl_conf;
 
-    /* Init cluster configuration structure. */
-    pi_cluster_conf_init(&cl_conf);
-    cl_conf.id = 0;                /* Set cluster ID. */
-    /* Configure & open cluster. */
-    pi_open_from_conf(&cluster_dev, &cl_conf);
-
-    pi_perf_conf(1 << PI_PERF_CYCLES | 1 << PI_PERF_ACTIVE_CYCLES);
-    pi_perf_reset();
-    pi_perf_start();
-
-    if (pi_cluster_open(&cluster_dev))
-    {
-        printf("Cluster open failed !\n");
-        pmsis_exit(-1);
-    }
-
-    pi_perf_stop();
-    uint32_t cycles = pi_perf_read(PI_PERF_ACTIVE_CYCLES);
-    uint32_t tim_cycles = pi_perf_read(PI_PERF_CYCLES);
-    printf("Perf : %d cycles Timer : %d cycles\n", cycles, tim_cycles);
-
-    /* Prepare cluster task and send it to cluster. */
-    struct pi_cluster_task cl_task;
-
-    pi_cluster_send_task_to_cl(&cluster_dev, pi_cluster_task(&cl_task, cluster_delegate, NULL));
-
-    pi_cluster_close(&cluster_dev);
 
     printf("Test success !\n");
 
-    pmsis_exit(errors);
+    pmsis_exit(0);
 }
 
 /* Program Entry. */
 int main(void)
 {
-    printf("\n\n\t *** PMSIS HelloWorld ***\n\n");
-    return pmsis_kickoff((void *) helloworld);
+    printf("\n\n\t *** PMSIS Camera Example Test ***\n\n");
+    return pmsis_kickoff((void *) test_camera);
 }
 
