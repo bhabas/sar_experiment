b114155f9e3fe341b054eddf24dca95468bb55a3
bhabas <bhabas@psu.edu>
2023-05-03 11:57:06 -0400

    Playing with more capture stuff...

--- AI_Deck_Apps/cam_test/cam_test.c
+++ AI_Deck_Apps/cam_test/cam_test.c
@@ -14,6 +14,7 @@
 #define CAM_WIDTH 162
 #define CAM_HEIGHT 122
 
+static pi_task_t task1;
 static unsigned char *imgBuff;
 static struct pi_device camera;
 static pi_buffer_t buffer;
@@ -21,6 +22,11 @@ static pi_buffer_t buffer;
 // Performance menasuring variables
 static uint32_t start = 0;
 static uint32_t captureTime = 0;
+uint32_t capture_arr[5] = {0};
+uint32_t image = 0;
+
+static EventGroupHandle_t evGroup;
+#define CAPTURE_DONE_BIT (1 << 0)
 
 static int open_pi_camera_himax(struct pi_device *device)
 {
@@ -49,20 +55,21 @@ static int open_pi_camera_himax(struct pi_device *device)
 }
 
 
+static void capture_done_cb(void *arg)
+{
+  xEventGroupSetBits(evGroup, CAPTURE_DONE_BIT);
+}
+
+
 void start_example(void)
 {
     printf("-- Starting Camera Test --\n");
     pi_perf_conf(1 << PI_PERF_CYCLES);
 
-    if (open_pi_camera_himax(&camera))
-    {
-        printf("Failed to open camera\n");
-        return;
-    }
-
     uint32_t imgSize = 0;
     uint32_t resolution = CAM_WIDTH * CAM_HEIGHT;
     uint32_t captureSize = resolution * sizeof(unsigned char);
+    evGroup = xEventGroupCreate();
 
     imgBuff = (unsigned char *)pmsis_l2_malloc(captureSize);
     if (imgBuff == NULL)
@@ -71,37 +78,53 @@ void start_example(void)
         return;
     }
 
+    if (open_pi_camera_himax(&camera))
+    {
+        printf("Failed to open camera\n");
+        return;
+    }
+
+
     pi_buffer_init(&buffer, PI_BUFFER_TYPE_L2, imgBuff);
     pi_buffer_set_format(&buffer, CAM_WIDTH, CAM_HEIGHT, 1, PI_BUFFER_FORMAT_GRAY);
     pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
 
-    uint8_t i = 0;
-    while (i < 5)
+    
+
+
+    printf("Camera Start...\n");
+    pi_perf_start();
+    start = pi_perf_read(PI_PERF_CYCLES);
+    while (image < 5)
     {
-        // pi_perf_start();
-        // start = pi_perf_read(PI_PERF_CYCLES);
-        uint32_t time_before = pi_time_get_us();
+        
+        
+        pi_camera_capture_async(&camera, imgBuff, resolution, pi_task_callback(&task1, capture_done_cb, NULL));
         pi_camera_control(&camera, PI_CAMERA_CMD_START, 0);
-        pi_camera_capture(&camera,imgBuff,resolution);
+        // pi_camera_capture(&camera,imgBuff,resolution);
         // captureTime = pi_perf_read(PI_PERF_CYCLES) - start;
+        // capture_arr[image] = captureTime;
+        xEventGroupWaitBits(evGroup, CAPTURE_DONE_BIT, pdTRUE, pdFALSE, (TickType_t)portMAX_DELAY);
         pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
 
-        // pi_time_wait_us(500);
-        // pi_perf_stop();
-        // pi_perf_reset();
-        // printf("\n");
-        vTaskDelay(5);
-        i++;
+        vTaskDelay(4);
+        
+        image++;
+
     }
-    printf("dsfsdf\n\n");
-    
-    
-
-    
-
-
+    pi_perf_stop();
+    printf("Camera End...\n");
+    captureTime = pi_perf_read(PI_PERF_CYCLES) - start;
+    printf("Capture_Time %d\n",captureTime);
     
 
+
+    for (uint8_t i = 0; i < 5; i++)
+    {
+        printf("Image %d = %d cycles\n",i,capture_arr[i]);
+
+    }
+   
 
     pmsis_exit(0);
 
