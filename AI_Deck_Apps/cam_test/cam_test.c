e315f712e2b47f8ba1d1062588da993f27104848
bhabas <bhabas@psu.edu>
2023-05-07 18:28:38 -0400

    Changed FPS counting to be time vs image based

--- AI_Deck_Apps/cam_test/cam_test.c
+++ AI_Deck_Apps/cam_test/cam_test.c
@@ -13,7 +13,7 @@
 #define CAM_WIDTH 162
 #define CAM_HEIGHT 122
 #define CLOCK_FREQ 250000000
-#define CAPTURE_NUM 5
+#define CAPTURE_NUM 2
 
 // CAMERA BUFFERS AND TASKS
 static struct pi_device camera;
@@ -28,12 +28,18 @@ static volatile uint8_t done2 = 0;
 static pi_buffer_t buffer2;
 static pi_task_t task2;
 
+volatile uint8_t img_num_sync = 0;
+volatile uint8_t img_num_async = 0;
+
+volatile uint32_t clock_cycles_sync = 0;
+volatile uint32_t clock_cycles_async = 0;
+
+
 
 // PERFORMANCE MEASURING VARIABLES
-static uint32_t start_capture = 0;
-static uint32_t captureTime = 0;
-uint32_t capture_arr[CAPTURE_NUM] = {0};
-uint32_t capture_count = 0;
+
+
+
 uint32_t resolution = CAM_WIDTH * CAM_HEIGHT;
 uint32_t captureSize = CAM_WIDTH * CAM_HEIGHT * sizeof(unsigned char);
 
@@ -64,12 +70,12 @@ static int open_pi_camera_himax(struct pi_device *device)
 
 static void capture_done_cb1(void *arg)
 {
-    done1 = 1;
+    done1++;
 }
 
 static void capture_done_cb2(void *arg)
 {
-    done2 = 1;
+    done2++;
 
 }
 
@@ -109,62 +115,68 @@ void cam_example(void)
     pi_buffer_init(&buffer2, PI_BUFFER_TYPE_L2, imgBuff2);
     pi_buffer_set_format(&buffer2, CAM_WIDTH, CAM_HEIGHT, 1, PI_BUFFER_FORMAT_GRAY);
 
+    
 
-    // START PERFORMANCE CAPTURE
-    printf("Camera Start...\n");
+
+    // STANDARD CAMERA CAPTURE
+    printf("Sync Camera Start...\n");
     pi_perf_stop();
     pi_perf_reset();
 
-
-    while (capture_count < CAPTURE_NUM)
+    pi_perf_start();
+    pi_camera_control(&camera, PI_CAMERA_CMD_START, 0);
+    while (pi_perf_read(PI_PERF_CYCLES) < CLOCK_FREQ)
     {
-        // RESET DONE FLAGS
+        pi_camera_capture(&camera, imgBuff1, resolution);
+        img_num_sync++;
+    }
+    pi_perf_stop();
+    pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
+
+    clock_cycles_sync = pi_perf_read(PI_PERF_CYCLES);
+    printf("Sync Camera End...\n\n");
+
+
+
+    // ASYNC CAMERA CAPTURE
+    printf("Async Camera Start...\n");
+    pi_perf_stop();
+    pi_perf_reset();
+
+    pi_perf_start();
+    pi_camera_control(&camera, PI_CAMERA_CMD_START, 0);
+    while (pi_perf_read(PI_PERF_CYCLES) < CLOCK_FREQ)
+    {
         done1 = 0;
-        done2 = 0;
-        
-        // INITIALIZE CAMERA CAPTURE TASKS
-
-
-        // START CAMERA AND WAIT TILL IMAGE BUFFERS ARE FILLED
-        pi_perf_start();
-        pi_camera_control(&camera, PI_CAMERA_CMD_START, 0);
-        // pi_camera_capture(&camera, imgBuff1, resolution);
         pi_camera_capture_async(&camera, imgBuff1, resolution, pi_task_callback(&task1, capture_done_cb1, NULL));
-        // pi_camera_capture_async(&camera, imgBuff2, resolution, pi_task_callback(&task2, capture_done_cb2, NULL));
-        while(done1 == 0)
+
+        while (done1 == 0)
         {
             pi_yield();
         }
-        pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
-        pi_perf_stop();
-
-        capture_arr[capture_count] = pi_perf_read(PI_PERF_CYCLES);
-        pi_perf_reset();
-
-        // RECORD CYCLES TO TAKE BOTH IMAGES
-        vTaskDelay(60); // Needs delay for an unknown reason
-
-
-        capture_count++;
+        
+        img_num_async++;
     }
-    
-    
+    pi_perf_stop();
+    pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
 
-    printf("Camera End...\n");
-    
+    clock_cycles_async = pi_perf_read(PI_PERF_CYCLES);
+    printf("Async Camera End...\n\n");
+
 
     // PRINT CAPTURE TIMES AND FPS
-    for (uint8_t i = 0; i < CAPTURE_NUM; i++)
-    {
-        float FPS = ((float)CLOCK_FREQ/(float)capture_arr[i]);
-        printf("Capture Set %d = %d cycles\n",i,capture_arr[i]);
-        printf("Capture Set %d = %.3f FPS\n",i,FPS);
+    float FPS_sync = ((float)CLOCK_FREQ/(float)clock_cycles_sync)*img_num_sync;
+    printf("Sync Capture: %d cycles\n",clock_cycles_sync);
+    printf("Sync Capture: %d images\n",img_num_sync);
+    printf("Sync Capture: %.3f FPS\n",FPS_sync);
 
+    printf("\n");
 
-    }
-    // printf("Total Capture Time: %d\n",captureTime);
-    // printf("FPS: %.3f\n",FPS);
-   
+    float FPS_async = ((float)CLOCK_FREQ/(float)clock_cycles_async)*img_num_async;
+    printf("Async Capture: %d cycles\n",clock_cycles_async);
+    printf("Async Capture: %d images\n",img_num_async);
+    printf("Async Capture: %.3f FPS\n",FPS_async);
+
 
     pmsis_exit(0);
 
