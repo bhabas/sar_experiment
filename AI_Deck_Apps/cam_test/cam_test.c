2ed6ca4dd80589ecc8db0b3568706ccc995a53f9
bhabas <bhabas@psu.edu>
2023-05-08 10:31:57 -0400

    Removed Sync capture and extra image buffer

--- AI_Deck_Apps/cam_test/cam_test.c
+++ AI_Deck_Apps/cam_test/cam_test.c
@@ -28,17 +28,9 @@ volatile uint8_t done1 = 0;
 pi_buffer_t buffer1;
 
 
-uint8_t *imgBuff2;
-volatile uint8_t done2 = 0;
-pi_buffer_t buffer2;
-pi_task_t task2;
-
 
 // PERFORMANCE MEASURING VARIABLES
-volatile uint8_t img_num_sync = 0;
 volatile uint8_t img_num_async = 0;
-
-volatile uint32_t clock_cycles_sync = 0;
 volatile uint32_t clock_cycles_async = 0;
 
 
@@ -71,12 +63,6 @@ static void capture_done_cb1(void *arg)
     img_num_async++;
 }
 
-static void capture_done_cb2(void *arg)
-{
-    done2++;
-    img_num_async++;
-
-}
 
 
 void cam_example(void)
@@ -104,38 +90,6 @@ void cam_example(void)
     pi_buffer_init(&buffer1, PI_BUFFER_TYPE_L2, imgBuff1);
     pi_buffer_set_format(&buffer1, CAM_WIDTH, CAM_HEIGHT, 1, PI_BUFFER_FORMAT_GRAY);
 
-    // INITIALIZE BUFFER 2
-    imgBuff2 = (uint8_t *)pmsis_l2_malloc(BUFFER_SIZE);
-    if (imgBuff2 == NULL)
-    {
-        printf("Failed to allocate memory_2 for image \n");
-        return;
-    }
-    pi_buffer_init(&buffer2, PI_BUFFER_TYPE_L2, imgBuff2);
-    pi_buffer_set_format(&buffer2, CAM_WIDTH, CAM_HEIGHT, 1, PI_BUFFER_FORMAT_GRAY);
-
-    
-
-
-    // STANDARD CAMERA CAPTURE
-    printf("Sync Camera Start...\n");
-    pi_perf_stop();
-    pi_perf_reset();
-
-    pi_perf_start();
-    pi_camera_control(&camera, PI_CAMERA_CMD_START, 0);
-    while (pi_perf_read(PI_PERF_CYCLES) < CLOCK_FREQ)
-    {
-        pi_camera_capture(&camera, imgBuff1, BUFFER_SIZE);
-        img_num_sync++;
-    }
-    pi_perf_stop();
-    pi_camera_control(&camera, PI_CAMERA_CMD_STOP, 0);
-
-    clock_cycles_sync = pi_perf_read(PI_PERF_CYCLES);
-    printf("Sync Camera End...\n\n");
-
-
 
     // ASYNC CAMERA CAPTURE
     printf("Async Camera Start...\n");
@@ -147,7 +101,6 @@ void cam_example(void)
     while (pi_perf_read(PI_PERF_CYCLES) < CLOCK_FREQ)
     {
         done1 = 0;
-        done2 = 0;
 
         pi_task_t task1;
         pi_camera_capture_async(&camera, imgBuff1, BUFFER_SIZE, pi_task_callback(&task1, capture_done_cb1, NULL));
@@ -155,7 +108,7 @@ void cam_example(void)
 
         while (true)
         {
-            if (done1 >= 1 || done2 >= 1)
+            if (done1 >= 1)
             {
                 break;
             }
@@ -174,15 +127,6 @@ void cam_example(void)
     clock_cycles_async = pi_perf_read(PI_PERF_CYCLES);
     printf("Async Camera End...\n\n");
 
-
-    // PRINT CAPTURE TIMES AND FPS
-    float FPS_sync = ((float)CLOCK_FREQ/(float)clock_cycles_sync)*img_num_sync;
-    printf("Sync Capture: %d cycles\n",clock_cycles_sync);
-    printf("Sync Capture: %d images\n",img_num_sync);
-    printf("Sync Capture: %.3f FPS\n",FPS_sync);
-
-    printf("\n");
-
     float FPS_async = ((float)CLOCK_FREQ/(float)clock_cycles_async)*img_num_async;
     printf("Async Capture: %d cycles\n",clock_cycles_async);
     printf("Async Capture: %d images\n",img_num_async);
@@ -200,7 +144,7 @@ int main(void)
 
     // Increase the FC freq to 250 MHz
     pi_freq_set(PI_FREQ_DOMAIN_FC, CLOCK_FREQ);
-    pi_pmu_voltage_set(PI_PMU_DOMAIN_FC, 1200); // Not sure on why set voltage?
+    __pi_pmu_voltage_set(PI_PMU_DOMAIN_FC, 1200); // Not sure on why set voltage?
 
     return pmsis_kickoff((void *)cam_example);
 }
