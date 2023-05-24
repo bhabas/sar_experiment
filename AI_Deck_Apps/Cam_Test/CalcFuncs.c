911aea3c58a1c34ffa29d8c7f26b504a38c6e52e
bhabas <bhabas@psu.edu>
2023-05-24 09:50:51 -0400

    Created new script to build full app

---
+++ AI_Deck_Apps/Cam_Test/CalcFuncs.c
@@ -0,0 +1,143 @@
+#include "CalcFuncs.h"
+
+void printVal(int32_t val)
+{
+    printf("Val: %d\n",val);           
+
+}
+
+void print_image_int32(int32_t* ImgBuff, uint8_t Cam_Width, uint8_t Cam_Height)
+{
+    for (int i = 0; i < Cam_Height; i++) {
+        for (int j = 0; j < Cam_Width; j++) {
+            printf("%3d ",ImgBuff[i*Cam_Width + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+}
+
+void print_image_uint8(uint8_t* ImgBuff, uint8_t Cam_Width, uint8_t Cam_Height)
+{
+    for (int i = 0; i < Cam_Height; i++) {
+        for (int j = 0; j < Cam_Width; j++) {
+            printf("%3d ",ImgBuff[i*Cam_Width + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+void convolve2D(uint8_t* img, int32_t* result, int32_t* kernel, int32_t startRow, int32_t endRow, int32_t stride)
+{
+    for (int32_t v_p = startRow; v_p <= endRow; v_p += stride)
+    {
+        for (int32_t u_p = 1; u_p < CAM_WIDTH - 1; u_p += stride)
+        {       
+            int32_t sum = 0;
+            for (int32_t i = 0; i <= 2; i++)
+            {
+                for (int32_t j = 0; j <= 2; j++)
+                {
+                    // Handle image boundaries
+                    if (v_p + i-1 < 0 || v_p + i-1 >= CAM_HEIGHT)
+                    {
+                        continue;
+                    }
+
+                    int32_t curPos = (v_p + i-1) * CAM_WIDTH + (u_p + j-1);
+                    int32_t kerPos = i*3 + j;
+                    sum += img[curPos] * kernel[kerPos];            
+                }
+            }
+            result[v_p*CAM_WIDTH + u_p] = sum;
+        }
+    }
+}
+
+int32_t dotProduct(int32_t* Vec1, int32_t* Vec2, int32_t size)
+{
+    int32_t result = 0;
+    for (int i = 0; i < size; i++)
+    {
+        result += Vec1[i] * Vec2[i];
+    }
+    return result;
+}
+
+void radialGrad(uint8_t* img, int32_t* result, int32_t* G_up, int32_t* G_vp, int32_t startRow, int32_t endRow, int32_t stride)
+{
+    for (int32_t v_p = startRow; v_p <= endRow; v_p += stride)
+    {
+        for (int32_t u_p = 1; u_p < CAM_WIDTH - 1; u_p += stride)
+        {
+            int32_t curPos = v_p*CAM_WIDTH + u_p;
+            result[curPos] = (2*u_p - CAM_WIDTH + 1)*G_up[curPos];
+        }
+    }
+}
+
+void temporalGrad(uint8_t* Cur_img_buff, uint8_t* Prev_img_buff, int32_t* result, int32_t startRow, int32_t endRow, int32_t stride)
+{
+    for (int i = startRow; i <= endRow; i += stride)
+    {
+        for (int j = 1; j < CAM_WIDTH-1; j += stride)
+        {
+            result[i*CAM_WIDTH + j] = Cur_img_buff[i*CAM_WIDTH + j] - Prev_img_buff[i*CAM_WIDTH + j];
+        }
+    }
+}
+
+void convolve2DSeparable(uint8_t* img, int32_t* result, int32_t* Kv, int32_t* Kh, int32_t startRow, int32_t numRows)
+{
+
+    // int32_t* temp = (int32_t*) pi_l2_malloc(CAM_WIDTH * sizeof(int32_t));
+    // int32_t* temp = (int32_t*) pi_cl_l1_malloc(&CL_device,CAM_WIDTH * sizeof(int32_t));
+    int32_t temp[CAM_WIDTH] = {0};
+
+
+
+
+    // Ensure we don't go beyond the image height
+    int32_t endRow = startRow + numRows;
+    if (endRow > CAM_HEIGHT) {
+        endRow = CAM_HEIGHT;
+    }
+
+
+
+    // Loop through the rows
+    for (int32_t r = startRow; r < endRow; ++r)
+    {
+        // Vertical convolution pass
+        for (int32_t c = 0; c < CAM_WIDTH - 0; ++c) // Skip the first and last column
+        {
+            temp[c] = 0;
+            for (int32_t kr = -1; kr <= 1; ++kr)
+            {
+                // Skip out of bounds
+                if (r+kr < 0 || r+kr >= CAM_HEIGHT) continue;
+                
+                // Apply vertical kernel
+                temp[c] += Kv[kr+1] * img[(r+kr)*CAM_WIDTH + c];
+            }
+        }
+
+        // Horizontal convolution pass, ensuring we don't convolve the left and right borders
+        for (int32_t c = 1; c < CAM_WIDTH - 1; ++c) // Skip the first and last column
+        {
+            result[r*CAM_WIDTH + c] = 0;
+            for (int32_t kc = -1; kc <= 1; ++kc)
+            {
+                // Apply horizontal kernel
+                result[r*CAM_WIDTH + c] += Kh[kc+1] * temp[c+kc];
+            }
+        }
+    }
+
+    // pi_cl_l1_free(&CL_device,temp,CAM_WIDTH * sizeof(int32_t));
+    // pi_l2_free(temp,CAM_WIDTH * sizeof(int32_t));
+
+}
---
+++ AI_Deck_Apps/Cam_Test/Cam_Processing.h
@@ -0,0 +1,132 @@
+#pragma once 
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+
+/* PMSIS includes */
+#include "pmsis.h"
+
+/* PMSIS BSP includes. */
+#include "bsp/bsp.h"
+#include "bsp/camera.h"
+
+#include "CalcFuncs.h"
+#include "params.h"
+
+
+// CAMERA BUFFERS AND TASKS
+struct pi_device Cam_device;
+struct pi_device CL_device;
+
+// CLUSTER SETUP
+
+uint32_t time_before = 0;
+uint32_t time_after = 0;
+
+
+uint8_t* ImgBuff[NUM_BUFFERS];
+
+volatile int current_idx = 0;
+volatile int next_idx = 0;
+
+
+int32_t G_up[CAM_WIDTH*CAM_HEIGHT];
+int32_t G_vp[CAM_WIDTH*CAM_HEIGHT];
+int32_t G_rp[CAM_WIDTH*CAM_HEIGHT];
+int32_t G_tp[CAM_WIDTH*CAM_HEIGHT];
+
+
+// PERFORMANCE MEASURING VARIABLES
+volatile uint8_t buffer_index = 0;
+volatile uint8_t img_num_async = 0;
+
+typedef struct ClusterImageData{
+    uint8_t  Rows_Per_Core;
+    uint8_t* Cur_img_buff;
+    uint8_t* Prev_img_buff;
+    int32_t  UART_array[10];
+
+    int32_t* DP_Vec1;
+    int32_t* DP_Vec2;
+    int32_t  DP_Sum_array[NUM_CORES];
+    int32_t* DP_Sum;
+} ClusterImageData_t;
+
+int32_t Ku[9] = {-1, 0, 1,
+                 -2, 0, 2,
+                 -1, 0, 1};
+int32_t Ku_v[3] = { 1, 2, 1};
+int32_t Ku_h[3] = {-1, 0, 1};
+
+int32_t Kv[9] = {-1,-2,-1,
+                  0, 0, 0,
+                  1, 2, 1};
+
+int32_t Kv_v[3] = {-1, 0, 1};
+int32_t Kv_h[3] = { 1, 2, 1};
+
+
+void delegate_GradCalcs(void *arg);
+void CL_GradCalcs(void *arg);
+void delegate_DotProducts(void *arg);
+void CL_DotProduct(void *arg);
+
+
+
+
+static int32_t open_pi_camera_himax(struct pi_device *device)
+{
+    // CAMERA CONFIG
+    struct pi_himax_conf cam_config;
+    pi_himax_conf_init(&cam_config);
+    cam_config.format = PI_CAMERA_QQVGA;
+
+    // OPEN CAMERA
+    pi_open_from_conf(device, &cam_config);
+    if (pi_camera_open(device))
+        return -1;
+
+    // ROTATE CAMERA IMAGE
+    pi_camera_control(&Cam_device, PI_CAMERA_CMD_START, 0);
+
+ 
+    uint8_t set_value = 3;
+    uint8_t reg_value;
+    pi_camera_reg_set(&Cam_device, IMG_ORIENTATION, &set_value);
+    pi_time_wait_us(500000);
+    pi_camera_reg_get(&Cam_device, IMG_ORIENTATION, &reg_value);
+
+    if (set_value != reg_value)
+    {
+        printf("Failed to rotate Cam_device image\n");
+        return -1;
+    }
+                
+    pi_camera_control(&Cam_device, PI_CAMERA_CMD_STOP, 0);
+    // pi_camera_control(device, PI_CAMERA_CMD_AEG_INIT, 0);
+    pi_time_wait_us(1000000); // Give time for Cam_device to adjust exposure
+
+
+    return 0;
+}
+
+static int32_t open_cluster(struct pi_device *device)
+{
+    struct pi_cluster_conf CL_Config;
+
+    // CLUSTER CONFIG
+    pi_cluster_conf_init(&CL_Config);
+    CL_Config.id = 0;
+    
+    // OPEN CLUSTER
+    pi_open_from_conf(&CL_device, &CL_Config);
+    if (pi_cluster_open(device))
+        return -1;
+
+    pi_freq_set(PI_FREQ_DOMAIN_CL, CLOCK_FREQ_CL);
+
+    return 0;
+}
