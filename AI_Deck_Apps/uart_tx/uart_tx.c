94e41e8eca8cf46b8503eb8b8fb5128bfb78dedf
bhabas <bhabas@psu.edu>
2023-07-10 20:09:05 -0400

    Sending byte array of data

--- AI_Deck_Apps/uart_tx/uart_tx.c
+++ AI_Deck_Apps/uart_tx/uart_tx.c
@@ -1,85 +1,71 @@
-// Include necessary libraries
 #include "stdlib.h"
 #include "stdio.h"
 #include "pmsis.h"
-
-// Macro to reverse bytes in 32-bit number
 #define reverse_bytes_32(num) ( ((num & 0xFF000000) >> 24) | ((num & 0x00FF0000) >> 8) | ((num & 0x0000FF00) << 8) | ((num & 0x000000FF) << 24) )
 
-// Initialize control characters and value to be sent
-int32_t valueToSend = 1234;
 uint8_t delimiter = '\1';
 uint8_t escape = '\2';
+int32_t valuesToSend[10] = {1234, 2345, 3456, 4567, 5678, 6789, 7890, 8901, 9012, 10123}; 
 
-// Initialize result variable for function return codes
-int result;
-
-// Function to test UART on GAP8
 static void test_gap8(void)
 {
     printf("Entering main controller...\n");
 
-    // Initialize and configure UART
+    // set configurations in uart
     struct pi_uart_conf conf;
     struct pi_device device;
-    
     pi_uart_conf_init(&conf);
     conf.baudrate_bps = 115200;
     conf.enable_tx = 1;
     conf.enable_rx = 0;
 
-    // Open UART with the specified configuration
+    // Open uart
     pi_open_from_conf(&device, &conf);
     printf("[UART] Open\n");
-
     if (pi_uart_open(&device))
     {
         printf("[UART] open failed !\n");
         pmsis_exit(-1);
     }
 
-    // Allocate buffer for storing the value to send
-    uint8_t *buffer = pmsis_l2_malloc(6 * sizeof(uint8_t));  // Maximum possible size (each byte could be escaped and then the delimiter)
+    pi_uart_open(&device);
+
+    // Make sure to allocate enough space
+    uint8_t *buffer = pmsis_l2_malloc(10 * 6 * sizeof(uint8_t));  
     int bufferSize = 0;
 
-    // Reference the bytes of the value to send
-    // Convert valueToSend to byte string format
-    uint8_t *valueBytes = (uint8_t *)&valueToSend;
-    
-    // Go through each byte and handle control characters
-    for (size_t i = 0; i < sizeof(int32_t); i++) {
-        if (valueBytes[i] == delimiter || valueBytes[i] == escape) {
-            buffer[bufferSize++] = escape;
-            buffer[bufferSize++] = valueBytes[i] + '\2';
-        } 
-        else {
-            buffer[bufferSize++] = valueBytes[i];
+    for (size_t valueIndex = 0; valueIndex < 10; ++valueIndex) {
+        int32_t valueToSend = valuesToSend[valueIndex];
+        uint8_t *valueBytes = (uint8_t *)&valueToSend;
+
+        for (size_t i = 0; i < sizeof(int32_t); i++) {
+            if (valueBytes[i] == delimiter || valueBytes[i] == escape) {
+                buffer[bufferSize++] = escape;
+                buffer[bufferSize++] = valueBytes[i] + '\2';
+            } else {
+                buffer[bufferSize++] = valueBytes[i];
+            }
         }
+
+        buffer[bufferSize++] = delimiter;
     }
 
-    // Add the delimiter at the end
-    buffer[bufferSize++] = delimiter;
-
-    // Continuous loop for sending the value
-    while(1)
-    {
-        result = pi_uart_write(&device, buffer, bufferSize);
+    // while(1)
+    // {
+        int result = pi_uart_write(&device, buffer, bufferSize);
 
         if (result == 0) {
-            printf("Value successfully sent!\n");
-        } 
-        else {
-            printf("Failed to send value, error code: %d\n", result);
+            printf("Values successfully sent!\n");
+        } else {
+            printf("Failed to send values, error code: %d\n", result);
         }
 
-        // Wait for 500 milliseconds before sending the value again
         pi_time_wait_us(500000);
-    }
+    // }
 
     pmsis_exit(0);
 }
 
-// Main function to kickoff the program
 int main(void)
 {
     return pmsis_kickoff((void *)test_gap8);
--- CF_Apps/uart_rx/src/uart_rx.c
+++ CF_Apps/uart_rx/src/uart_rx.c
@@ -1,71 +1,47 @@
-// Import necessary standard libraries
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-// Include necessary project specific files
 #include "app.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "uart1.h"
 #include "log.h"
 
-// Define DEBUG_MODULE for debugging
 #define DEBUG_MODULE "HELLOWORLD"
 #include "debug.h"
 #include "log.h"
 
-// Set control characters for UART transmission
 uint8_t delimiter = '\1';
 uint8_t escape = '\2';
 
-// Initialize array to hold received bytes, set maximum size to 6
-uint8_t receivedBytes[6];
-// Index to track position in the receivedBytes array
+uint8_t receivedBytes[6];  
 int byteIndex = 0;
-// Boolean flag to track if the last received byte was the escape character
 bool lastByteWasEscape = false;
-// Integer to hold the final received value
 int32_t value = 0;
 
 void appMain() {
-    // Print debug message
     DEBUG_PRINT("Waiting for activation ...\n");
-    // Initialize UART communication
     uart1Init(115200);
 
-    // Main loop
     while(1) {
-        // Delay task execution for 500 milliseconds
-        vTaskDelay(M2T(500));
+        // vTaskDelay(M2T(500));
 
         uint8_t byte;
-        // Receive one byte from UART with default timeout
         uart1GetBytesWithDefaultTimeout(1, &byte);
 
-        // Print each received byte in hexadecimal format
-        consolePrintf("Received Byte: %02X\n", byte); 
+        // consolePrintf("Received Byte: %02X\n", byte); 
 
-        // Check conditions based on received byte
         if (lastByteWasEscape) {
-            // If the last byte was an escape character, subtract 2 from current byte
-            // and store it in the buffer, reset escape flag
             receivedBytes[byteIndex++] = byte - '\2';
             lastByteWasEscape = false;
-        } 
-        else if (byte == escape) {
-            // If the current byte is an escape character, set escape flag to true
+        } else if (byte == escape) {
             lastByteWasEscape = true;
-        } 
-        else if (byte == delimiter) {
-            // If the current byte is a delimiter, copy the bytes collected till now 
-            // as a complete int32_t value into `value` and print it, reset the byte index
+        } else if (byte == delimiter) {
             memcpy(&value, receivedBytes, sizeof(int32_t));
             consolePrintf("Received Value: %ld\n", value);
-            byteIndex = 0;
-        } 
-        else {
-            // If current byte is none of the above, just add it to the buffer
+            byteIndex = 0;  // Reset byteIndex for the next value
+        } else {
             receivedBytes[byteIndex++] = byte;
         }
     }
