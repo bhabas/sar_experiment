6ff66a3f13ad6e7f5940d41e9a616defd184931e
bhabas <bhabas@psu.edu>
2023-07-29 18:03:50 -0400

    This method of handling gradients is slow

--- OpenMV_H7/Buffer_Difference.py
+++ OpenMV_H7/Buffer_Difference.py
@@ -13,7 +13,7 @@ sensor.set_hmirror(True)
 ## ALLOW SYSTEM TO UPDATE CAMERA SETTINGS
 sensor.skip_frames(time = 1000) # Let new settings take affect.
 clock = time.clock() # Tracks FPS.
-##sensor.set_framerate(10)
+sensor.set_auto_exposure(True, exposure_us=30_000) # make smaller to go faster
 
 ## CAPTURE INITIAL IMAGE
 img_prev = sensor.snapshot().copy()
@@ -22,27 +22,56 @@ t_prev = time.time_ns()
 t_curr = time.time_ns()
 t_delta = t_curr - t_prev
 
+i = 0
+
+K_up = [-1,  0,  1,
+        -2,  0,  2,
+        -1,  0,  1]
+
+K_vp = [-1, -2, -1,
+         0,  0,  0,
+         1,  2,  1]
+kernel_size = 1
+
 while True:
 
     clock.tick()
 
 
     ## CAPTURE CURRENT IMAGE
-    img_curr = sensor.snapshot().copy()
+    img_curr = sensor.snapshot()
     t_curr = time.time_ns()
+    i += 1
 
+
+
+    img_G_tp = img_curr.copy()  # Create a copy of current image
+    img_G_up = img_curr.copy()
+    img_G_vp = img_curr.copy()
+    img_G_rp = img_curr.copy()
 
     ## SUBTRACT CUR_IMG FROM PREV_IMG
-    img_diff = img_curr.copy()  # Create a copy of current image
-    img_diff.sub(img_prev)
+    img_G_tp.sub(img_prev)
+    img_G_up.morph(kernel_size, K_up)
+    img_G_vp.morph(kernel_size, K_up)
+
+#    pix_sum = 0
+    for u_p in range(0,64):
+        for v_p in range(0,64):
+            pixel_val = (2*u_p - 64 + 1)*img_G_up.get_pixel(u_p,v_p) + (2*v_p - 64 + 1)*img_G_vp.get_pixel(u_p,v_p)
+            img_G_rp.set_pixel(u_p,v_p,pixel_val)
+
+
+#    print(pix_sum)
+
+
+
     t_delta = t_curr - t_prev
-    print(t_delta/1_000_000)
-    print(clock.fps())
-    print(clock.avg())
+    print(f"t_delta: {t_delta/1_000_000:.3f} ms \t idx: {i:04d}")
 
 
     ## SET IMAGE TO BE SEND TO IDE BUFFER
-    img_diff.copy(copy_to_fb=True)
+    img_G_up.copy(copy_to_fb=True)
 
     # Now, we need to replace img_prev with img_current for the next iteration
     img_prev = img_curr
