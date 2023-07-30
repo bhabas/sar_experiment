76eb057f56b93548888d2e7876f53aee3d4d9dec
bhabas <bhabas@psu.edu>
2023-07-30 14:50:10 -0400

    I think framework of calc is here but need to test on actual images to verify

--- OpenMV_H7/Buffer_Difference.py
+++ OpenMV_H7/Buffer_Difference.py
@@ -1,5 +1,6 @@
 import sensor, image, pyb, os, time
 from ulab import numpy as np
+from ulab import scipy as spy
 sensor.reset() # Initialize the camera sensor.
 
 ## SET SIZE AND PIXEL FORMAT
@@ -8,6 +9,8 @@ sensor.set_framesize(sensor.B64X64)
 
 N_up = 64
 N_vp = 64
+IW = 1.152e-3
+f = 0.66e-3
 
 ## ROTATE IMAGE
 sensor.set_transpose(True)
@@ -45,49 +48,71 @@ for u_p in range(0,N_up):
 for v_p in range(0,N_vp):
     v_p_dist[v_p,:] = (2*v_p-N_vp+1)
 
-
-
-#while True:
-
-#    print(u_p_dist)
-#    print(v_p_dist)
-#    time.sleep(0.1)
-
-
 while True:
 
     clock.tick()
 
-
     ## CAPTURE CURRENT IMAGE
     img_curr = sensor.snapshot()
     t_curr = time.time_ns()
-    img_bytearray = img_curr.bytearray() # Convert image to bytestring
+    t_delta = t_curr - t_prev
 
     img_G_tp = img_curr.copy()  # Create a copy of current image
     img_G_up = img_curr.copy()
     img_G_vp = img_curr.copy()
-    img_G_rp = img_curr.copy()
 
     ## SUBTRACT CUR_IMG FROM PREV_IMG AND PERFORM GRAD CONVOLUTIONS
     img_G_tp.sub(img_prev)
     img_G_up.morph(kernel_size, K_up)
     img_G_vp.morph(kernel_size, K_up)
 
-
+    ## CONVERT IMAGE BUFFERS TO ARRAYS
+    img_bytearray = img_curr.bytearray() # Convert image to bytestring
     G_tp = np.frombuffer(img_G_tp.bytearray(), dtype=np.uint8).reshape((N_up,N_vp))
     G_up = np.frombuffer(img_G_up.bytearray(), dtype=np.uint8).reshape((N_up,N_vp))
     G_vp = np.frombuffer(img_G_vp.bytearray(), dtype=np.uint8).reshape((N_up,N_vp))
     G_rp = np.zeros((N_up,N_vp),dtype=np.uint8)
+    G_rp = u_p_dist*G_up + v_p_dist*G_vp
 
-#    G_rp = u_p_dist*G_up + v_p_dist*G_vp
+    ## CALC DOT PRODUCTS
+    G_vp_vp = np.sum(G_vp*G_vp)
+    G_vp_up = np.sum(G_vp*G_up)
+    G_vp_rp = np.sum(G_vp*G_rp)
+    G_vp_tp = np.sum(G_vp*G_tp)
 
-    t_delta = t_curr - t_prev
+    G_up_up = np.sum(G_up*G_up)
+    G_up_rp = np.sum(G_up*G_rp)
+    G_up_tp = np.sum(G_up*G_tp)
+
+    G_rp_rp = np.sum(G_rp*G_rp)
+    G_rp_tp = np.sum(G_rp*G_tp)
+
+    ## SOLVE LEAST SQUARES PROBLEM
+    A = np.array([
+        [G_vp_vp, -G_vp_up, -IW/(2*N_up*f)*G_vp_rp],
+        [G_vp_up, -G_up_up, -IW/(2*N_up*f)*G_up_rp],
+        [G_vp_rp, -G_up_rp, -IW/(2*N_up*f)*G_rp_rp]
+    ])
+
+    b = np.array([
+        [G_vp_tp],
+        [G_up_tp],
+        [G_rp_tp]
+    ])*(8*IW/(f*N_up*t_delta))
+
+    A = np.array([[25, 15, -5], [15, 18,  0], [-5,  0, 11]])
+    b = np.array([9,-3,7])
+
+
+    ## SOLVE b VIA PSEUDO-INVERSE
+    try:
+        A = np.linalg.cholesky(A)
+        x = spy.linalg.cho_solve(A, b)
+#        print(x[:])
+    except:
+        print("Singular Matrix")
+
     print(f"t_delta: {t_delta/1_000_000:.3f} ms \t FPS_Avg: {clock.fps():.1f}")
-
-
-    ## SET IMAGE TO BE SEND TO IDE BUFFER
-    img_G_up.copy(copy_to_fb=True)
 
     # Now, we need to replace img_prev with img_current for the next iteration
     img_prev = img_curr
