0a17c104a02584c4061740bd91b0290bb284f0ed
bhabas <bhabas@psu.edu>
2023-09-04 10:51:40 -0400

    Global variable fix for GTC

--- CF_Apps/controller_GTC/src/Compress_States.c
+++ CF_Apps/controller_GTC/src/Compress_States.c
@@ -1,5 +1,10 @@
 #include "Compress_States.h"
 
+// Definition of the global instances
+States_Z_Struct States_Z;
+TrgStates_Z_Struct TrgStates_Z;
+SetPoints_Z_Struct SetPoints_Z;
+
 
 void compressStates(){
 
