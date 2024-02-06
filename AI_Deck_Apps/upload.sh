c639ccd71c412a890561bac6dba5a67344915014
bhabas <bhabas@psu.edu>
2024-02-06 18:11:47 -0500

    Cleared out old AI_Deck files

--- AI_Deck_Apps/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $# -lt 1 ]; then
-    echo "Please provide the app to build as the first argument."
-    exit 1
-fi
-
-cfloader flash $1/BUILD/GAP8_V2/GCC_RISCV_FREERTOS/target.board.devices.flash.img deck-bcAI:gap8-fw -w radio://0/80/2M/E7E7E7E701
